ac831a1f9bc24a1a1f78d9e170aea3b045cd9b7f
lalithl1993 <lalithl1993@gmail.com>
2016-12-29 12:50:34 +0530

    Add files via upload

---
+++ Gallery/gallery.php
@@ -0,0 +1,290 @@
+<?php
+include '..\database.php';
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width"/>
+<title>Abhyuday 3.0</title>
+<!-- CSS Files-->
+<link rel="shortcut icon" href="../images/final.png" type="image/png"/>
+<link rel="stylesheet" href="../stylesheets/style.css">
+<link rel="stylesheet" href="../stylesheets/homepage.css">
+<link rel="stylesheet" href="../stylesheets/skins/teal.css">
+<link rel="stylesheet" href="../stylesheets/responsive.css">   
+
+		<!-- Start VisualLightBox.com HEAD section -->
+
+		<link rel="stylesheet" href="gallery_files/vlb_files0/vlightbox0.css" type="text/css" />
+
+		<link rel="stylesheet" href="gallery_files/vlb_files0/visuallightbox.css" type="text/css" media="screen" />
+<script src="gallery_files/vlb_engine/jquery.min.js" type="text/javascript"></script>
+
+		<script src="gallery_files/vlb_engine/visuallightbox.js" type="text/javascript"></script>
+
+		<!-- End VisualLightBox.com HEAD section -->
+
+</head>
+<body>
+<!-- HIDDEN PANEL 
+================================================== -->
+<div id="panel">
+	<div class="row">
+		<div class="twelve columns">
+			<img src="../images/info.png" class="pics" alt="info">
+			<div class="infotext">
+				Welcome to MSRIT .
+			</div>
+		</div>
+	</div>
+</div>
+<p class="slide">
+	<a href="#" class="btn-slide"></a>
+</p>
+<!-- HEADER
+================================================== -->
+<div class="row">	
+		<div class="four columns">
+			<div class="logo">
+				<img src="../images/final.png " ></img>
+				<a href="../index.php"><h4>ABHYUDAY 3.0</h4></a>
+			</div>
+		</div>
+		<div class="eight columns noleftmarg">		
+			<nav id="nav-wrap">
+				<ul class="nav-bar sf-menu">
+				
+					<li >
+						<a href="../index.php">Home</a>
+					</li>
+					<li >
+						<a href="#">About US</a>
+					</li>
+					
+					<li>
+						<a href="../events.php">Events</a>
+					</li>
+					
+					<li>
+						<a href="../schedule.php">Schedule</a>	
+					</li>
+					
+					<li>
+						<a href="../register.php">Register</a>
+					<li>
+						<a href="#">Downloads</a>	
+					</li>
+					
+					<li class="current">
+						<a href="../contact.php">Contact Us</a>
+					</li>
+					
+				</ul>
+			</nav>
+		</div>	
+</div>
+
+<div class="clear">
+</div>
+<!-- SUBHEADER
+================================================== -->
+<div id="subheader">
+	<div class="row">
+		<div class="twelve columns">
+			<p align="center">
+				 GALLERY
+			</p>
+		</div>
+	</div>
+</div>
+<div class="hr">
+</div>
+<!-- CONTENT 
+================================================== -->
+<div class="row">
+	<div class="twelve columns ">
+		
+	<div class="winner">
+	<fieldset>
+	<legend>Winners of Abhyuday 2.0</legend>
+	<table >
+
+	<tr>
+		<td><a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0492.jpg" title="DSC_0492"><img src="gallery_files/vlb_thumbnails0/dsc_0492.jpg" alt="DSC_0492"/></a></td>
+		<td ><a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0494.jpg" title="DSC_0494"><img src="gallery_files/vlb_thumbnails0/dsc_0494.jpg" alt="DSC_0494"/></a></td>
+	</tr>
+	<tr>
+		<td style="text-align:center">MOUNT CARMEL COLLEGE - MCA</td>
+		<td style="text-align:center">KLE COLLEGE - BCA</td>
+	</tr>
+	</table>
+	</fieldset>
+	</div>
+
+
+ <br>
+
+	<div id="vlightbox0">
+	<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0007.jpg" title="DSC_0007"><img src="gallery_files/vlb_thumbnails0/dsc_0007.jpg" alt="DSC_0007"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0013.jpg" title="DSC_0013"><img src="gallery_files/vlb_thumbnails0/dsc_0013.jpg" alt="DSC_0013"/></a>
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_00273_2.jpg" title="DSC_0027-3 (2)"><img src="gallery_files/vlb_thumbnails0/dsc_00273_2.jpg" alt="DSC_0027-3 (2)"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0033.jpg" title="DSC_0033"><img src="gallery_files/vlb_thumbnails0/dsc_0033.jpg" alt="DSC_0033"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0042.jpg" title="DSC_0042"><img src="gallery_files/vlb_thumbnails0/dsc_0042.jpg" alt="DSC_0042"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0044.jpg" title="DSC_0044"><img src="gallery_files/vlb_thumbnails0/dsc_0044.jpg" alt="DSC_0044"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0045.jpg" title="DSC_0045"><img src="gallery_files/vlb_thumbnails0/dsc_0045.jpg" alt="DSC_0045"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0046.jpg" title="DSC_0046"><img src="gallery_files/vlb_thumbnails0/dsc_0046.jpg" alt="DSC_0046"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0052.jpg" title="DSC_0052"><img src="gallery_files/vlb_thumbnails0/dsc_0052.jpg" alt="DSC_0052"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0058.jpg" title="DSC_0058"><img src="gallery_files/vlb_thumbnails0/dsc_0058.jpg" alt="DSC_0058"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0060.jpg" title="DSC_0060"><img src="gallery_files/vlb_thumbnails0/dsc_0060.jpg" alt="DSC_0060"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0067.jpg" title="DSC_0067"><img src="gallery_files/vlb_thumbnails0/dsc_0067.jpg" alt="DSC_0067"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0084.jpg" title="DSC_0084"><img src="gallery_files/vlb_thumbnails0/dsc_0084.jpg" alt="DSC_0084"/></a>
+
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0103.jpg" title="DSC_0103"><img src="gallery_files/vlb_thumbnails0/dsc_0103.jpg" alt="DSC_0103"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0108.jpg" title="DSC_0108"><img src="gallery_files/vlb_thumbnails0/dsc_0108.jpg" alt="DSC_0108"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0109.jpg" title="DSC_0109"><img src="gallery_files/vlb_thumbnails0/dsc_0109.jpg" alt="DSC_0109"/></a>
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0117.jpg" title="DSC_0117"><img src="gallery_files/vlb_thumbnails0/dsc_0117.jpg" alt="DSC_0117"/></a>
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0140.jpg" title="DSC_0140"><img src="gallery_files/vlb_thumbnails0/dsc_0140.jpg" alt="DSC_0140"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0141.jpg" title="DSC_0141"><img src="gallery_files/vlb_thumbnails0/dsc_0141.jpg" alt="DSC_0141"/></a>
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0143.jpg" title="DSC_0143"><img src="gallery_files/vlb_thumbnails0/dsc_0143.jpg" alt="DSC_0143"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0144.jpg" title="DSC_0144"><img src="gallery_files/vlb_thumbnails0/dsc_0144.jpg" alt="DSC_0144"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0145.jpg" title="DSC_0145"><img src="gallery_files/vlb_thumbnails0/dsc_0145.jpg" alt="DSC_0145"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0146.jpg" title="DSC_0146"><img src="gallery_files/vlb_thumbnails0/dsc_0146.jpg" alt="DSC_0146"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0147.jpg" title="DSC_0147"><img src="gallery_files/vlb_thumbnails0/dsc_0147.jpg" alt="DSC_0147"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0152.jpg" title="DSC_0152"><img src="gallery_files/vlb_thumbnails0/dsc_0152.jpg" alt="DSC_0152"/></a>
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0159.jpg" title="DSC_0159"><img src="gallery_files/vlb_thumbnails0/dsc_0159.jpg" alt="DSC_0159"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0161.jpg" title="DSC_0161"><img src="gallery_files/vlb_thumbnails0/dsc_0161.jpg" alt="DSC_0161"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0162.jpg" title="DSC_0162"><img src="gallery_files/vlb_thumbnails0/dsc_0162.jpg" alt="DSC_0162"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0163.jpg" title="DSC_0163"><img src="gallery_files/vlb_thumbnails0/dsc_0163.jpg" alt="DSC_0163"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0164.jpg" title="DSC_0164"><img src="gallery_files/vlb_thumbnails0/dsc_0164.jpg" alt="DSC_0164"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0165.jpg" title="DSC_0165"><img src="gallery_files/vlb_thumbnails0/dsc_0165.jpg" alt="DSC_0165"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0166.jpg" title="DSC_0166"><img src="gallery_files/vlb_thumbnails0/dsc_0166.jpg" alt="DSC_0166"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0168.jpg" title="DSC_0168"><img src="gallery_files/vlb_thumbnails0/dsc_0168.jpg" alt="DSC_0168"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0169.jpg" title="DSC_0169"><img src="gallery_files/vlb_thumbnails0/dsc_0169.jpg" alt="DSC_0169"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0172.jpg" title="DSC_0172"><img src="gallery_files/vlb_thumbnails0/dsc_0172.jpg" alt="DSC_0172"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0173.jpg" title="DSC_0173"><img src="gallery_files/vlb_thumbnails0/dsc_0173.jpg" alt="DSC_0173"/></a>
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0186.jpg" title="DSC_0186"><img src="gallery_files/vlb_thumbnails0/dsc_0186.jpg" alt="DSC_0186"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0187.jpg" title="DSC_0187"><img src="gallery_files/vlb_thumbnails0/dsc_0187.jpg" alt="DSC_0187"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0190.jpg" title="DSC_0190"><img src="gallery_files/vlb_thumbnails0/dsc_0190.jpg" alt="DSC_0190"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0194.jpg" title="DSC_0194"><img src="gallery_files/vlb_thumbnails0/dsc_0194.jpg" alt="DSC_0194"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0196.jpg" title="DSC_0196"><img src="gallery_files/vlb_thumbnails0/dsc_0196.jpg" alt="DSC_0196"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0197.jpg" title="DSC_0197"><img src="gallery_files/vlb_thumbnails0/dsc_0197.jpg" alt="DSC_0197"/></a>
+
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0267.jpg" title="DSC_0267"><img src="gallery_files/vlb_thumbnails0/dsc_0267.jpg" alt="DSC_0267"/></a>
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0283.jpg" title="DSC_0283"><img src="gallery_files/vlb_thumbnails0/dsc_0283.jpg" alt="DSC_0283"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0290.jpg" title="DSC_0290"><img src="gallery_files/vlb_thumbnails0/dsc_0290.jpg" alt="DSC_0290"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0306.jpg" title="DSC_0306"><img src="gallery_files/vlb_thumbnails0/dsc_0306.jpg" alt="DSC_0306"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0312.jpg" title="DSC_0312"><img src="gallery_files/vlb_thumbnails0/dsc_0312.jpg" alt="DSC_0312"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0327.jpg" title="DSC_0327"><img src="gallery_files/vlb_thumbnails0/dsc_0327.jpg" alt="DSC_0327"/></a>
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0388.jpg" title="DSC_0388"><img src="gallery_files/vlb_thumbnails0/dsc_0388.jpg" alt="DSC_0388"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0403.jpg" title="DSC_0403"><img src="gallery_files/vlb_thumbnails0/dsc_0403.jpg" alt="DSC_0403"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0408.jpg" title="DSC_0408"><img src="gallery_files/vlb_thumbnails0/dsc_0408.jpg" alt="DSC_0408"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0409.jpg" title="DSC_0409"><img src="gallery_files/vlb_thumbnails0/dsc_0409.jpg" alt="DSC_0409"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0416.jpg" title="DSC_0416"><img src="gallery_files/vlb_thumbnails0/dsc_0416.jpg" alt="DSC_0416"/></a>
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_04212.jpg" title="DSC_0421-2"><img src="gallery_files/vlb_thumbnails0/dsc_04212.jpg" alt="DSC_0421-2"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0427.jpg" title="DSC_0427"><img src="gallery_files/vlb_thumbnails0/dsc_0427.jpg" alt="DSC_0427"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0429.jpg" title="DSC_0429"><img src="gallery_files/vlb_thumbnails0/dsc_0429.jpg" alt="DSC_0429"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0432.jpg" title="DSC_0432"><img src="gallery_files/vlb_thumbnails0/dsc_0432.jpg" alt="DSC_0432"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0434.jpg" title="DSC_0434"><img src="gallery_files/vlb_thumbnails0/dsc_0434.jpg" alt="DSC_0434"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0436.jpg" title="DSC_0436"><img src="gallery_files/vlb_thumbnails0/dsc_0436.jpg" alt="DSC_0436"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0438.jpg" title="DSC_0438"><img src="gallery_files/vlb_thumbnails0/dsc_0438.jpg" alt="DSC_0438"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0440.jpg" title="DSC_0440"><img src="gallery_files/vlb_thumbnails0/dsc_0440.jpg" alt="DSC_0440"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0441.jpg" title="DSC_0441"><img src="gallery_files/vlb_thumbnails0/dsc_0441.jpg" alt="DSC_0441"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0442.jpg" title="DSC_0442"><img src="gallery_files/vlb_thumbnails0/dsc_0442.jpg" alt="DSC_0442"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0446.jpg" title="DSC_0446"><img src="gallery_files/vlb_thumbnails0/dsc_0446.jpg" alt="DSC_0446"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0453.jpg" title="DSC_0453"><img src="gallery_files/vlb_thumbnails0/dsc_0453.jpg" alt="DSC_0453"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0458.jpg" title="DSC_0458"><img src="gallery_files/vlb_thumbnails0/dsc_0458.jpg" alt="DSC_0458"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0462.jpg" title="DSC_0462"><img src="gallery_files/vlb_thumbnails0/dsc_0462.jpg" alt="DSC_0462"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0464.jpg" title="DSC_0464"><img src="gallery_files/vlb_thumbnails0/dsc_0464.jpg" alt="DSC_0464"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0467.jpg" title="DSC_0467"><img src="gallery_files/vlb_thumbnails0/dsc_0467.jpg" alt="DSC_0467"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0472.jpg" title="DSC_0472"><img src="gallery_files/vlb_thumbnails0/dsc_0472.jpg" alt="DSC_0472"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0476.jpg" title="DSC_0476"><img src="gallery_files/vlb_thumbnails0/dsc_0476.jpg" alt="DSC_0476"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0483.jpg" title="DSC_0483"><img src="gallery_files/vlb_thumbnails0/dsc_0483.jpg" alt="DSC_0483"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0485.jpg" title="DSC_0485"><img src="gallery_files/vlb_thumbnails0/dsc_0485.jpg" alt="DSC_0485"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0486.jpg" title="DSC_0486"><img src="gallery_files/vlb_thumbnails0/dsc_0486.jpg" alt="DSC_0486"/></a>
+
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0505001.jpg" title="DSC_0505-001"><img src="gallery_files/vlb_thumbnails0/dsc_0505001.jpg" alt="DSC_0505-001"/></a>
+<a class="vlightbox0" href="gallery_files/vlb_images0/dsc_0516.jpg" title="DSC_0516"><img src="gallery_files/vlb_thumbnails0/dsc_0516.jpg" alt="DSC_0516"/></a>
+
+	</div>
+
+	<script src="gallery_files/vlb_engine/vlbdata0.js" type="text/javascript"></script>
+
+	<!-- End VisualLightBox.com BODY section -->
+
+<!-- CATEGORIES-->
+		
+	
+	
+	
+	</div>
+</div>
+<div class="hr">
+</div>
+<!-- FOOOTER 
+================================================== -->
+<div id="footer">
+	<footer class="row">
+	
+	<div class="four columns">
+		<h1>GET SOCIAL</h1>
+		<div class="social facebook">
+			<a href="#"></a>
+		</div>
+		<div class="social twitter">
+			<a href="#"></a>
+		</div>
+		<div class="social deviantart">
+			<a href="#"></a>
+		</div>
+		<div class="social flickr">
+			<a href="#"></a>
+		</div>
+		<div class="social dribbble">
+			<a href="#"></a>
+		</div>
+	</div>
+	
+	</footer>
+</div>
+<div class="copyright">
+	<div class="row">
+		<div class="six columns">
+			 &copy;<span class="small"> Copyright 2016 Abhyuday 3.0</span>
+		</div>
+		<div class="six columns">
+			<span class="small floatright"> Designed and Developed by Anand and Lalith</span>
+		</div>
+	</div>
+</div>
+<!-- JAVASCRIPTS 
+================================================== -->
+<!-- Javascript files placed here for faster loading -->
+<script src="javascripts/foundation.min.js"></script>
+<script src="javascripts/jquery.cycle.js"></script>
+<script src="javascripts/app.js"></script>
+<script src="javascripts/modernizr.foundation.js"></script>
+<script src="javascripts/slidepanel.js"></script>
+<script src="../javascripts/scrolltotop.js"></script>
+<script src="javascripts/hoverIntent.js"></script>
+<script src="javascripts/superfish.js"></script>
+<script src="javascripts/responsivemenu.js"></script>
+<script src="javascripts/jquery.tweet.js"></script>
+<script src="javascripts/twitterusername.js"></script>
+<script src="javascripts/jquery.isotope.min.js"></script>
+<script src="javascripts/jquery.prettyPhoto.js"></script>
+<script src="javascripts/custom.js"></script>
+</body>
+</html>
